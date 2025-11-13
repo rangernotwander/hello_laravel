7bdd913681131ba69a3d115effb571a56a59f9b5
cooper <cooper163php@gmail.com>
2025-11-13 14:12:03 +0800

    添加注册静态页面

---
+++ resources/views/users/create.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+@section('title','注册')
+
+@section('content')
+<div class="container mt-5">
+    <h1>注册</h1>
+    <p>这是用户注册页面的占位内容，下一节我们将添加注册表单。</p>
+</div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -2,15 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\StaticPagesController; // ← 这一行必须加上！
-// Route::get('/', function () {
-//     return view('static_pages.home'); // ← 必须返回继承了布局的视图
-// })->name('home');
+use App\Http\Controllers\UsersController; // ← 这一行必须加上！
 
-// Route::get('/help', function () {
-//     return '<div class="container mt-4"><h2>帮助中心</h2><p>这里是帮助内容...</p></div>';
-// });
-
+//静态也main
 Route::get('/',[StaticPagesController::class,'home'])->name('home');
 Route::get('/help',[StaticPagesController::class,'help'])->name('help');
 Route::get('/about',[StaticPagesController::class,'about'])->name('about');
 
+
+//用户注册
+Route::get('/signup',[UsersController::class,'create'])->name('signup');
